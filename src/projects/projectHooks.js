39dd672aeb93b38838dc1123ff7332e687b85990
erickNjuki <njukierick@gmail.com>
2023-07-25 21:11:05 +0300

    Hooks

---
+++ src/projects/projectHooks.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { projectAPI } from './projectAPI.js';
+
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+
+export function useProjects() {
+  const [page, setPage] = useState(0);
+
+  let queryInfo = useQuery(['projects', page], () => projectAPI.get(page + 1), {
+    keepPreviousData: true,
+    staleTime: 5000,
+  });
+
+  return { ...queryInfo, page, setPage };
+}
+
+export function useSaveProject() {
+  const queryClient = useQueryClient();
+  return useMutation((project) => projectAPI.put(project), {
+    onSuccess: () => queryClient.invalidateQueries('projects'),
+  });
+}
